0d6c09bc6b9eee12efe4c96bb913ff74e5f05c9a
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-02 12:47:24 +0100

    Update path.ts for compatibility with the installed Expo version

--- app/axios/path.ts
+++ app/axios/path.ts
@@ -1,27 +1,32 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 
 
 const path = axios.create({
-    baseURL: 'http://localhost:7000/api',
-    responseType: 'json',
-    withCredentials: true,
+    baseURL: 'http://localhost:7000/api/',
 })
-
 path.interceptors.request.use(
     async (config) => {
-    const token = await AsyncStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-        config.headers['Content-Type'] = 'multipart/form-data';
-    }
-    return config;
-},
-    (error) => {
-    return Promise.reject(error);
-}
+        // console.log('config', config);
+        // const token = await AsyncStorage.getItem('token');
 
+        // if (token) {
+        //     config.headers.Authorization = `Bearer ${token}`;
+        // }
+
+        if (config.data instanceof FormData) {
+            config.headers["Content-Type"] = "multipart/form-data";
+        } else {
+            config.headers["Content-Type"] = "application/json";
+        }
+
+        return config;
+    },
+    function (err) {
+        console.error("Axios Request Error:", err);
+        return Promise.reject(err);
+    }
 );
 
 
