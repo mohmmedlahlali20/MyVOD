d0a568ba030ef5f21c7ec9d3fcccc222894a169c
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-02 12:48:01 +0100

    Update register.tsx for compatibility with the installed Expo version

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,9 +1,60 @@
-import React from "react"
-import { View, Text, TextInput, TouchableOpacity, ScrollView } from "react-native"
-import { Link, Stack } from "expo-router"
-import { StatusBar } from "expo-status-bar"
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, ScrollView, Alert } from "react-native";
+import { Link, Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import path from "../axios/path"; 
+import { register } from "../redux/userSlice";
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
 export default function Register() {
+  const dispatch = useDispatch();
+  const [form, setForm] = useState({
+    firstname: "Mohammed",
+    lastname: "Lahlali",
+    email: "Mohammepklhpd@gmail.com",
+    password: "password123",
+    confirmPassword: "password123",
+  });
+
+  const handleChange = (name: string, value: string) => {
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+
+  const handleRegister = async () => {
+    if (!form.firstname || !form.lastname || !form.email || !form.password) {
+      Alert.alert("Erreur", "Veuillez remplir tous les champs !");
+      return;
+    }
+    if (form.password !== form.confirmPassword) {
+      Alert.alert("Erreur", "Les mots de passe ne correspondent pas !");
+      return;
+    }
+
+  
+    try {
+      const res = await axios.post("http://localhost:7000/api/auth/register", {
+        firstname: form.firstname,
+        lastname: form.lastname,
+        email: form.email,
+        password: form.password
+      }, {
+        headers: { "Content-Type": "application/json" },
+      });
+      
+      console.log("fuck");
+      console.log(res);
+      dispatch(register(res.data));
+      Alert.alert("Succès", "Inscription réussie !");
+    } catch (error: any) {
+      console.log("Error details:", error);
+      Alert.alert("Erreur", error.response?.data?.message || "Erreur dans la requête !");
+    }
+  };
+  
+
+
   return (
     <ScrollView className="flex-1 bg-black">
       <Stack.Screen
@@ -21,49 +72,59 @@ export default function Register() {
           <View>
             <Text className="text-sm font-medium text-white mb-1">First Name</Text>
             <TextInput
-              className="w-full px-3 py-2 border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full px-3 py-2 border border-white rounded-md text-white"
               placeholder="John"
+              value={form.firstname}
+              onChangeText={(text) => handleChange("firstname", text)}
             />
           </View>
 
           <View>
             <Text className="text-sm font-medium text-white mb-1">Last Name</Text>
             <TextInput
-              className="w-full px-3 py-2 border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full px-3 py-2 text-white border border-white rounded-md"
               placeholder="Doe"
+              value={form.lastname}
+              onChangeText={(text) => handleChange("lastname", text)}
             />
           </View>
 
           <View>
             <Text className="text-sm font-medium text-white mb-1">Email</Text>
             <TextInput
-              className="w-full px-3 py-2 border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full px-3 py-2 text-white border border-white rounded-md"
               placeholder="john.doe@example.com"
               keyboardType="email-address"
               autoCapitalize="none"
+              value={form.email}
+              onChangeText={(text) => handleChange("email", text)}
             />
           </View>
 
           <View>
-            <Text className="text-sm font-medium text-white mb-1">Password</Text>
+            <Text className="text-sm text-white font-medium mb-1">Password</Text>
             <TextInput
-              className="w-full px-3 py-2 border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full px-3 py-2 border border-white rounded-md text-white"
               placeholder="********"
               secureTextEntry
+              value={form.password}
+              onChangeText={(text) => handleChange("password", text)}
             />
           </View>
 
           <View>
             <Text className="text-sm font-medium text-white mb-1">Confirm Password</Text>
             <TextInput
-              className="w-full px-3 py-2 border border-white rounded-md focus:outline-none focus:ring-2 focus:ring-red-500"
+              className="w-full px-3 py-2 text-white border border-white rounded-md"
               placeholder="********"
               secureTextEntry
+              value={form.confirmPassword}
+              onChangeText={(text) => handleChange("confirmPassword", text)}
             />
           </View>
         </View>
 
-        <TouchableOpacity className="mt-6 bg-red-600 py-3 rounded-md" activeOpacity={0.8}>
+        <TouchableOpacity className="mt-6 bg-red-600 py-3 rounded-md" activeOpacity={0.8} onPress={handleRegister}>
           <Text className="text-center text-white font-semibold">Register</Text>
         </TouchableOpacity>
 
@@ -77,6 +138,5 @@ export default function Register() {
         </Text>
       </View>
     </ScrollView>
-  )
+  );
 }
-
