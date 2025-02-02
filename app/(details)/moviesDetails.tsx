27bbe54502f210c68f0fc45b641eca3685c30402
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-02 20:54:20 +0100

    Add details page layout and components

---
+++ app/(details)/moviesDetails.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function moviesDetails() {
+  return (
+    <View>
+      <Text>moviesDetails</Text>
+    </View>
+  )
+}
