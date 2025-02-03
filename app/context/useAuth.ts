ec27e1f595db497051dfecfd18b2a56a6c93d8ad
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-03 15:55:00 +0100

    Add context files for authentication

---
+++ app/context/useAuth.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter, useSegments } from "expo-router";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+
+
+export function useAuth() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const token = useSelector((state: RootState) => state.user.token);
+  const user = useSelector((state: RootState) => state.user.user);
+
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const storedToken = await AsyncStorage.getItem("token");
+      console.log('====================================');
+      console.log('storedToken', storedToken);
+      console.log('====================================');
+
+      if (!storedToken && !token) {
+          router.push("/login");
+          return;
+      }
+
+      setLoading(false);
+    };
+
+    checkAuth();
+  }, [token]);
+
+  return { loading, user };
+}
