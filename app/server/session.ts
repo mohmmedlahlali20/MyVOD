f8b913d26b387735120bddda0f5b856b98903e8d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-06 16:04:42 +0100

    Add session.ts for server-side session handling

---
+++ app/server/session.ts
@@ -0,0 +1,15 @@
+import path from "../axios/path";
+
+
+
+
+const getSessionsForMovie = async (movieId:string)=>{
+   const response = await path.get(`seance/getseance/${movieId}`);
+
+   return response.data
+}
+
+
+
+
+export {getSessionsForMovie};
