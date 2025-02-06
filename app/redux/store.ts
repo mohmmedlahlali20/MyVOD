1561e3e7422131e6287c5c200e59671fa6fec12e
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-06 16:04:40 +0100

    Update store.ts for compatibility with the installed Expo version

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authreducer from './userSlice';
 import Moviesreducer from './movieSlice';
+import seanceReducer from './seanceSlice';
+import ReservationsReducer from './reservationSlice'
 
 const store = configureStore({
       reducer: {
         user: authreducer,
-        movies: Moviesreducer
+        movies: Moviesreducer,
+        seance: seanceReducer,
+        reservations: ReservationsReducer
     }
 })
 
